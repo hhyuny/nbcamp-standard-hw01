ebe467918188d3ef51ab761d7e3d26d11dbc2310
hhyuny <tiamozz77@gmail.com>
2024-08-14 15:10:45 +0900

    HW 01 업로드

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header";
+import Content from "./components/Content";
+import Footer from "./components/Footer";
+import "./App.css";
+
+function App() {
+  return (
+    <section className="intro-section">
+      <Header />
+      <Content />
+      <Footer />
+    </section>
+  );
+}
+
+export default App;
